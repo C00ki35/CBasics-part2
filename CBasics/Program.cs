e04cf75c857a3b5b6cbb9fb07a91a3a1a5e2165a
Paul <pgcdigit@gmail.com>
2020-07-02 09:49:19 +0100

    increase age using value or reference

--- CBasics/Program.cs
+++ CBasics/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CBasics
 {
+
+    public class Person
+    {
+        public int Age;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
 
-            Console.WriteLine("Testing");
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
