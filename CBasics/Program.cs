f9b4a8e2734a1ffe99067720e125843a5560b34e
Paul <pgcdigit@gmail.com>
2020-07-03 08:55:17 +0100

    for-foreach-while loops- basic

--- CBasics/Program.cs
+++ CBasics/Program.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.Win32;
+﻿
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 
 namespace CBasics
 {
@@ -12,68 +8,44 @@ namespace CBasics
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = false;
-            float price = (isGoldCustomer) ? 20.45f : 44.35f;
-            Console.WriteLine(price);
 
-            var season = Season.Autumn;
-
-            switch (season)
+            for (var i = 1; i <= 10; i++)
             {
-                case Season.Autumn:
-                case Season.Summer:
-                    Console.WriteLine("Its Autumn");
-                    break;
-                case Season.Winter:
-                    Console.WriteLine("Its Winter");
-                    break;
-                default:
-                    Console.WriteLine("No season");
-                    break;
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
 
-            //t1
-
-            var input = Console.ReadLine();
-            var number = Convert.ToInt32(input);
-            var isValid = (number > 0 && number < 11) ? "Valid": "Invalid";
-            Console.WriteLine(isValid);
-
-            //t2
-            var numberOne = 5;
-            var numberTwo = 9;
-
-            var whichIsGreater = (numberOne > numberTwo) ? numberOne : numberTwo;
-            Console.WriteLine(whichIsGreater);
-
-            //t3
-            var width = 300;
-            var height = 600;
-
-            var isLandscape = (width > height) ? "Landscape" : "Portrait";
-            Console.WriteLine(isLandscape);
-
-            //t4
-            const int speedLimit = 25;
-            var carSpeed = 30;
-            var fine = (carSpeed - speedLimit);
-
-            if (fine > 0)
+            var name = "Something here";
+            foreach (var item in name)
             {
-                var points = (fine / 5);
-                if(points > 12)
-                    Console.WriteLine("Suspended");
-                else
-                    Console.WriteLine("You have got " + points + " points on your license.");
-            } else
+                Console.WriteLine(item);
+            }
+
+            var names = new string[3] { "Dan", "Simon", "Frank" };
+            foreach (var person in names)
             {
-                Console.WriteLine("No issues");
+                Console.WriteLine(person);
             }
 
 
+            var x = 0;
+            while(x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
 
+            while (true)
+            {
+                Console.Write("Enter your name: ");
+                var input = Console.ReadLine();
 
-
+                if (!String.IsNullOrWhiteSpace(input)) { 
+                    break;
+                }
+            }
         }   
     }
 }
