fd922a12e915451711331177f306bf57f38bc579
Paul <pgcdigit@gmail.com>
2020-07-07 17:25:31 +0100

    DateTime and TimeSpan

--- CBasics/Program.cs
+++ CBasics/Program.cs
@@ -1,8 +1,5 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Xml;
 
 namespace CBasics
 {
@@ -10,131 +7,38 @@ namespace CBasics
     {
         static void Main(string[] args)
         {
-            ////Arrays
-            //var numbers = new[] { 4,3,4,2,66,8 };
-            //Console.WriteLine(numbers.Length);
+            //DateTime display
+            var dateTime = new DateTime(2020, 07, 07);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
 
-            //var indexofEight = Array.IndexOf(numbers, 8);
-            //Console.WriteLine(indexofEight);
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
 
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            var twentyMintuesAgo = now.AddMinutes(-20);
 
-            ////Clear sets given number of items to zero, false or null
-            //Array.Clear(numbers, 0, 2);
-            //foreach (var number in numbers)
-            //{
-            //    Console.WriteLine(number);
-            //}
+            Console.WriteLine(twentyMintuesAgo);
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToString("yy-MM-dd HH:mm"));
 
-            //Console.WriteLine();
+            //Time Span
 
-            ////Copy method 
-            //var another = new int[3];
-            //Array.Copy(numbers, another, 3);
-            //foreach (var item in another)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan2 = TimeSpan.FromHours(1);
 
-            ////Sort
-            //Array.Sort(numbers);
-            //foreach (var n in numbers)
-            //{
-            //    Console.WriteLine(n);
-            //}
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(5);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
 
-            ////Reverse
-            //Console.WriteLine("Reversed");
-            //Array.Reverse(numbers);
-            //foreach (var n in numbers)
-            //{
-            //    Console.WriteLine(n);
-            //}
-
-            //LISTS - are mutable
-            //var numbersList = new List<int>() { 1, 2, 3, 5 };
-            //numbersList.Add(9);
-            //numbersList.AddRange(new int[3] { 6, 77, 77 });
-
-            //foreach (var n in numbersList)
-            //{
-            //    Console.WriteLine(n);
-            //}
-            //Console.WriteLine("The index of number 2 is: " + numbersList.IndexOf(2));
-            //Console.WriteLine("The index of number 2 is: " + numbersList.LastIndexOf(2));
-            //Console.WriteLine("Total number of numbers: " + numbersList.Count);
-
-            //Excercises
-            //var friends = new List<String>();
-            //while (true)
-            //{
-            //    Console.WriteLine("Add a friend: ");
-            //    var input = Console.ReadLine();
-
-
-            //    friends.Add(input);
-            //    var totalLikes = friends.Count;
-
-            //    if (input == "")
-            //    {
-            //            Console.WriteLine("0 likes");
-            //        break;
-            //    }
-            //    if (totalLikes == 1)
-            //    {
-            //        Console.WriteLine(friends[0] + " likes your post");
-            //    }
-            //    if (totalLikes == 2)
-            //    {
-            //        Console.WriteLine(friends[0] + ", " + friends[1] + " likes your post");
-            //    }
-            //    if(totalLikes > 2)
-            //    {
-            //        Console.WriteLine(friends[0] + ", " + friends[1] + " and " + (friends.Count - 2) + " other(s) like your post.");
-
-
-            //    }
-
-
-
-            //Console.WriteLine("Enter your name: ");
-            //var input = Console.ReadLine();
-            //char[] chars = input.ToCharArray();
-            //Array.Reverse(chars);
-
-            //var reversed = "";
-            //for (int i = 0; i < chars.Length; i++)
-            //{
-            //    reversed += chars[i];
-            //}
-
-            //Console.WriteLine(reversed);
-
-            //var friends = new List<String>();
-
-
-            var givenNumbers = new List<int>();
-            while (true)
-            {
-                Console.WriteLine("Enter a number: ");
-                var numberInput = Console.ReadLine();
-                var isFound = givenNumbers.Exists(n => n == Convert.ToInt16(numberInput));
-                if (!isFound)
-                {
-                    givenNumbers.Add(Convert.ToInt32(numberInput));
-                }
-                else
-                {
-                    Console.WriteLine("please select another number");
-                }
-                if (givenNumbers.Count == 5)
-                {
-                    givenNumbers.Sort();
-                    foreach (var number in givenNumbers)
-                    {
-                        Console.WriteLine(number);
-                    }
-                }
-            }
+            //Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            //Subtract
+            Console.WriteLine("Add Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)
+            Console.WriteLine("ToString" + timeSpan);
         }
     }
 }
